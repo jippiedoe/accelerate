169817938a30a895b7a17c02931c6f72fb8404ce
David <d.p.vanbalen@students.uu.nl>
2020-04-02 14:29:33 +0200

    replaced the horizontal GADT with a 'skip' in the index transform gadt

--- src/Data/Array/Accelerate/Trafo/NewFusion/NewInterpreter.hs
+++ src/Data/Array/Accelerate/Trafo/NewFusion/NewInterpreter.hs
@@ -43,7 +43,7 @@ import           Control.Monad
 -- import           Data.Typeable
 -- import           System.IO.Unsafe               ( unsafePerformIO )
 -- import           Text.Printf                    ( printf )
-import           Unsafe.Coerce
+-- import           Unsafe.Coerce
 import           Prelude                 hiding ( (!!)
                                                 , sum
                                                 )
@@ -60,7 +60,7 @@ import           Data.Array.Accelerate.AST
 --                                                 ( sizeOfScalarType
 --                                                 , sizeOfSingleType
 --                                                 )
-import           Data.Array.Accelerate.Array.Data
+import           Data.Array.Accelerate.Array.Data -- because we're doing a lot of mutating, and I'm paranoid, I try to avoid 'unsafeIndexArrayData' in favour of 'unsafeReadArrayData'
 -- import qualified          Data.Array.Accelerate.Array.Representation
 --                                                 ( SliceIndex(..) )
 import           Data.Array.Accelerate.Array.Sugar
@@ -75,7 +75,7 @@ import Data.IORef
 import           Control.Monad.State.Strict
 import Data.Foldable
 -- import Data.Array.Accelerate.Type
-
+import Data.Type.Bool
 
 data ValArr env where
   EmptyEnv :: ValArr ()
@@ -88,43 +88,37 @@ instance ShapeIsh Shape
 instance ShapeIsh Neg1
 
 
-data HorizontalFusion left right res where
-  Done :: HorizontalFusion () () ()
-  Righty :: HorizontalFusion () b b -> HorizontalFusion () (b, Array sh e) (b, Array sh e)
-  Lefty  :: HorizontalFusion a  b c -> HorizontalFusion (a, Array sh e) b  (c, Array sh e)
+-- data HorizontalFusion left right res where
+--   Done :: HorizontalFusion () () ()
+--   Righty :: HorizontalFusion () b b -> HorizontalFusion () (b, Array sh e) (b, Array sh e)
+--   Lefty  :: HorizontalFusion a  b c -> HorizontalFusion (a, Array sh e) b  (c, Array sh e)
 
-
+-- 'from' is a "subset" of 'to'
 data IdxSpaceTransform from to where
   Id :: IdxSpaceTransform a a
-  Fn :: (Shape sh, Shape sh')
+  Fn :: (ShapeIsh sh, ShapeIsh sh')
      => Int -- offset
      -> IdxSpaceTransform from to
-     -> IdxSpaceTransform (from, Array sh e) (to, Array sh' e)
+     -> IdxSpaceTransform (from, Array sh e) (to, Array sh' e) -- usually, sh' == sh :. Int. But due to composition it can be nested deeper, and we also say that Neg1 is a 'subset' of Z
+  Skip :: ShapeIsh sh
+       => IdxSpaceTransform from to
+       -> IdxSpaceTransform from (to, Array sh e)
 
-getCurrent :: IdxSpaceTransform (env, x) (env', x') -> Int
-getCurrent Id = 0
-getCurrent (Fn i _) = i
 
-getRest :: IdxSpaceTransform (env, x) (env', x') -> IdxSpaceTransform env env'
-getRest Id = Id
-getRest (Fn _ x) = x
 
 compose :: IdxSpaceTransform b c -> IdxSpaceTransform a b -> IdxSpaceTransform a c
 compose Id x = x
 compose x Id = x
+compose (Skip x) y = Skip (compose x y)
+compose (Fn _ x) (Skip y) = Skip (compose x y)
 compose (Fn i ab) (Fn j bc) = Fn (i+j) (compose ab bc)
 
 infix 9 $*
 ($*) :: Int -> IdxSpaceTransform a b -> IdxSpaceTransform a b
 _ $* Id = Id
-i $* (Fn j f) = Fn (i*j) $ i $* f
+i $* (Fn j f) = Fn (i*j) (i $* f)
+i $* (Skip x) = Skip (i $* x)
 
--- transform :: IdxSpaceTransform (a, Array sh1 e) (b, Array sh2 e) -> Array sh1 e -> Array sh2 e
--- transform = undefined
-
-inverse :: IdxSpaceTransform a b -> IdxSpaceTransform b a
-inverse Id = Id
-inverse (Fn i x) = Fn (-i) (inverse x)
 
 --IR
 data IntermediateRep permanentIn tempIn out where
@@ -139,67 +133,83 @@ data IntermediateRep permanentIn tempIn out where
        -> IdxSpaceTransform tin tin' -- These IST's should represent the offset of the second iteration
        -> IdxSpaceTransform out out' -- (loop # 1), as they will get multiplied by the iteration counter
        -> IntermediateRep pin tin' out'
-  Simple :: LoopBody pin a b
+  Simple :: LoopBody pin a b x y
          -> IntermediateRep pin a b
 
   -- Vertical fusion
-  Before  :: LoopBody        pin a b
+  Before  :: ValArr b -- temporary result allocation place, the dimensions (concrete shapes) should be computable while constructing this IR
+          -> LoopBody        pin a b x 'True
           -> IntermediateRep pin b c
           -> IntermediateRep pin a c
-  After   :: IntermediateRep pin a b
-          -> LoopBody        pin b c
+  After   :: ValArr b -- temporary result allocation place, the dimensions (concrete shapes) should be computable while constructing this IR
+          -> IntermediateRep pin a b
+          -> LoopBody        pin b c 'True x
           -> IntermediateRep pin a c
 
   -- Diagonal fusion
-  Before' :: HorizontalFusion b c hor
-          -> LoopBody        pin a b
+  Before' :: IdxSpaceTransform b hor
+          -> IdxSpaceTransform c hor
+          -> LoopBody        pin a b x 'True
           -> IntermediateRep pin b c
           -> IntermediateRep pin a hor
-  After'  :: HorizontalFusion b c hor
+  After'  :: IdxSpaceTransform b hor
+          -> IdxSpaceTransform c hor
           -> IntermediateRep pin a b
-          -> LoopBody        pin b c
+          -> LoopBody        pin b c 'True x
           -> IntermediateRep pin a hor
 
   -- Horizontal fusion
-  Besides :: HorizontalFusion b c hor
-          -> LoopBody        pin a b
+  Besides :: IdxSpaceTransform b hor
+          -> IdxSpaceTransform c hor
+          -> LoopBody        pin a b x y
           -> IntermediateRep pin a c
           -> IntermediateRep pin a hor
 
-data LoopBody permIn tempIn out where
-  Base :: LoopBody  pin () ()
-  Take :: --(Shape sh1, Shape sh2, Elt a, Elt b)
-        LoopBody' pin (Array sh1 a) (Array sh2 b)
-       -> LoopBody  pin c d
-       -> LoopBody  pin (c, Array sh1 a) (d, Array sh2 b)
-  Drop :: LoopBody  pin a b
-       -> LoopBody  pin (a,x) (b,x)
+data LoopBody permIn tempIn out fusein fuseout where
+  Base :: LoopBody  pin () () 'True 'True
+  Take :: LoopBody' pin (Array sh1 a) (Array sh2 b)
+       -> LoopBody  pin c d fin fout
+       -> LoopBody  pin (c, Array sh1 a) (d, Array sh2 b) (fin && Canfuse sh1) (fout && Canfuse sh2)
+  Drop :: LoopBody  pin a b nfin fnout
+       -> LoopBody  pin (a,x) (b,x) nfin nfout
 
+-- arrays of dimension 'negative one' can't be fused vertically at that level, they have to be 'expanded' by a for-loop to dimension 0 first.
+-- think of folds: fusing with their input happens one loop deeper than fusing with their output
+-- note that arrays of dimension 'Neg1' can't actually exists, but we can create gadt's with a phantom type of 'Array Neg1 a', allowing us to 'IdxTransform' into it
+type family Canfuse (a :: *) = (b :: Bool) where
+  Canfuse DIM0 = 'True
+  Canfuse Neg1 = 'False
 
+-- the 'State s' requirements allow functions to keep track of their running index into the permIn, which doesn't have idxtransforms
 data LoopBody' permIn tempIn out where
   OneToOne :: (Elt a, Elt b)
            => IORef s
            -> (ValArr pin -> a -> State s b)
            -> LoopBody' pin (Array DIM0 a) (Array DIM0 b)
   ManyToOne :: (Elt a, Elt b)
-            => Int
-            -> IORef (s, Int)
+            => IORef (s, Int)
+            -> Int -- in size
             -> (ValArr pin -> a -> State s ())
             -> State s b
             -> LoopBody' pin (Array DIM0 a) (Array Neg1 b)
-           {-
-  -- state seems fairly useless here, but for consistency
-  OneToMany ::s -> ([Int] -> pin ->
-              a -> State s [b])
-            -> Int --outsize
-            -> LoopBody' pin (Array DIM0 a) (Array DIM1 b)
-  -- the Bool signifies whether this is the last 'a'
-  ManyToMany :: Int --insize
-             -> s -> ([Int] -> pin ->
-               a -> Bool -> State s (Maybe b))
-             -> Int --outsize
-             -> LoopBody' pin (Array DIM1 a) (Array DIM1 b)
--}
+  OneToMany :: (Elt a, Elt b)
+            => IORef (s, [b], Int)
+            -> Int -- out size
+            -> (ValArr pin -> a -> State s [b])
+            -> LoopBody' pin (Array Neg1 a) (Array DIM0 b)
+
+            -- TODO to accomodate functions resulting from something like a 'unfold . fold', we need either this constructor (and a way to deal with it), or IntermediateRep needs to allow composition of two loops
+            -- this generalisation might not be too common, but it SHOULD be allowed? Or is this an example of general composition inside of the outer loop, which is not the same as fusion across all dimensions?
+            -- not even sure whether the ILP 'allows' this fusion
+            -- there might be a way to allow IntermediateRep to contain two (or more?) loops inside of a loop while still statically avoiding non-fusion patterns?
+            -- Or we just have to promise to avoid them when constructing the IR
+  -- ManyToMany :: (Elt a, Elt b)
+  --            => IORef s
+  --            -> Int -- in size
+  --            -> (pin -> a -> State s (Maybe b))
+  --            -> Int -- out size
+  --            -> LoopBody' pin (Array DIM1 a) (Array DIM1 b)
+
 
 
 evalIR
@@ -217,117 +227,88 @@ evalIR'
   -> IdxSpaceTransform tInputs tInputs'
   -> IdxSpaceTransform outputs outputs'
   -> IO ()
-evalIR' Void                 _ _ _ _  Id = return ()
-evalIR' (For ir n ti' to')   p t o ti to = traverse_ (\i -> evalIR' ir p t o (i $* compose ti ti') (i $* compose to to')) [0..n-1]
-evalIR' (Simple body)        p t o ti to = evalLoopBody body p t o ti to
-evalIR' (Before  body ir)    p t o ti to = let temp = undefined in evalLoopBody body p t temp ti Id >> evalIR' ir p temp o Id to
-evalIR' (After   ir body)    p t o ti to = let temp = undefined in evalIR' ir p t temp ti Id >> evalLoopBody body p temp o Id to
--- evalIR' (Before' hf body ir) p t o ti to = let (tb, tc) = (makeLeftIdx hf to, makeRightIdx hf to)
---                                                a = evalLoopBody body p t ti tb in
---     fuseHorizontal (unsafeCoerce hf) <$> a <*> (a >>= \x -> evalIR' ir p x tb tc) -- unsafeCoerce because we need the hf at the 'real' array level, not the phantom one
--- evalIR' (After'  hf ir body) p t o ti to = let (tb, tc) = (makeLeftIdx hf to, makeRightIdx hf to)
---                                                a = evalIR' ir p t ti tb in
---     fuseHorizontal (unsafeCoerce hf) <$> a <*> (a >>= \x -> evalLoopBody body p x tb tc)
--- evalIR' (Besides hf body ir) p t o ti to = let (tb, tc) = (makeLeftIdx hf to, makeRightIdx hf to) in
---     fuseHorizontal (unsafeCoerce hf) <$> evalLoopBody body p t ti tb <*> evalIR' ir p t ti tc
-
-evalIR' _ _ _ _ _ _ = undefined -- TODO write the horizontal ones again
+evalIR' Void                   _ _ _ _  _  = return ()
+evalIR' (For ir n ti' to')     p t o ti to = traverse_ (\i -> evalIR' ir p t o (i $* compose ti ti') (i $* compose to to')) [0..n-1]
+evalIR' (Simple bdy)           p t o ti to = evalLB bdy p t o ti to
+evalIR' (Before  temp  bdy ir) p t o ti to = evalLB bdy p t temp ti Id >> evalIR' ir p temp o Id to
+evalIR' (After   temp  ir bdy) p t o ti to = evalIR' ir p t temp ti Id >> evalLB bdy p temp o Id to
+evalIR' (Before' bh ch bdy ir) p t o ti to = evalLB bdy p t o ti (compose to bh) >> evalIR' ir p o o (compose to bh) (compose to ch)
+evalIR' (After'  bh ch ir bdy) p t o ti to = evalIR' ir p t o ti (compose to bh) >> evalLB bdy p o o (compose to bh) (compose to ch)
+evalIR' (Besides bh ch bdy ir) p t o ti to = evalLB bdy p t o ti (compose to bh) >> evalIR' ir p t o ti (compose to ch)
 
 
 
--- the a', b' and c' should be the origins of a, b and c, so the two HF arguments should match
--- since this is always the case, we can unsafecoerce them? --TODO check
-makeLeftIdx :: HorizontalFusion a b c -> IdxSpaceTransform c c' -> IdxSpaceTransform a a'
-makeLeftIdx hf = makeLeftIdx' hf (unsafeCoerce hf)
-makeRightIdx :: HorizontalFusion a b c -> IdxSpaceTransform c c' -> IdxSpaceTransform b b'
-makeRightIdx hf = makeRightIdx' hf (unsafeCoerce hf)
+
+evalLB :: LoopBody pinputs tinputs outputs x y
+       -> ValArr pinputs
+       -> ValArr tinputs'
+       -> ValArr outputs'
+       -> IdxSpaceTransform tinputs tinputs'
+       -> IdxSpaceTransform outputs outputs'
+       -> IO ()
+evalLB Base _ x y Id Id = assign Id x y
+-- Copy the top array, and recurse
+evalLB (Drop     lb) p (PushEnv ts t) (PushEnv os o) Id       Id       = assignArr   0     t o >> evalLB lb p ts os Id Id
+evalLB (Drop     lb) p (PushEnv ts t) (PushEnv os o) (Fn i a) Id       = assignArr   (-i)  t o >> evalLB lb p ts os a  Id
+evalLB (Drop     lb) p (PushEnv ts t) (PushEnv os o) Id       (Fn j b) = assignArr   j     t o >> evalLB lb p ts os Id b
+evalLB (Drop     lb) p (PushEnv ts t) (PushEnv os o) (Fn i a) (Fn j b) = assignArr   (i-j) t o >> evalLB lb p ts os a  b
+-- Evaluate the LoopBody' and recurse
+evalLB (Take lb' lb) p (PushEnv ts t) (PushEnv os o) Id       Id       = evalLB' lb' 0 0 p t o >> evalLB lb p ts os Id Id
+evalLB (Take lb' lb) p (PushEnv ts t) (PushEnv os o) (Fn i a) Id       = evalLB' lb' i 0 p t o >> evalLB lb p ts os a  Id
+evalLB (Take lb' lb) p (PushEnv ts t) (PushEnv os o) Id       (Fn j b) = evalLB' lb' 0 j p t o >> evalLB lb p ts os Id b
+evalLB (Take lb' lb) p (PushEnv ts t) (PushEnv os o) (Fn i a) (Fn j b) = evalLB' lb' i j p t o >> evalLB lb p ts os a  b
+-- Recurse
+evalLB lb p (PushEnv ts _) o (Skip idxt) idxo = evalLB lb p ts o idxt idxo
+evalLB lb p t (PushEnv os _) idxt (Skip idxo) = evalLB lb p t os idxt idxo
+
+evalLB (Take _ _) _ EmptyEnv _ x _ = case x of {} -- There is no IdxSpaceTransform
+evalLB (Drop   _) _ EmptyEnv _ x _ = case x of {} -- possible for these cases,
+evalLB (Take _ _) _ _ EmptyEnv _ x = case x of {} -- so this satisfies GHC's
+evalLB (Drop   _) _ _ EmptyEnv _ x = case x of {} -- incomplete pattern check. :)
 
 
 
-makeLeftIdx' :: HorizontalFusion a b c
-             -> HorizontalFusion a' b' c'
-             -> IdxSpaceTransform c c'
-             -> IdxSpaceTransform a a'
-makeLeftIdx' Done Done _ = Id
-makeLeftIdx' (Righty _) (Righty _) _ = Id
-makeLeftIdx' (Lefty l) (Lefty l') (Fn f x) = Fn f $ makeLeftIdx' l l' x
-makeLeftIdx' _ _ _ = error "makeLeftIdx fusion arguments don't match"
-
-makeRightIdx' :: HorizontalFusion a b c
-              -> HorizontalFusion a' b' c'
-              -> IdxSpaceTransform c c'
-              -> IdxSpaceTransform b b'
-makeRightIdx' Done Done _ = Id
-makeRightIdx' (Righty r) (Righty r') (Fn f x) = Fn f $ makeRightIdx' r r' x
-makeRightIdx' (Lefty l) (Lefty l') (Fn _ x) = makeRightIdx' l l' x
-makeRightIdx' _ _ _ = error "makeRightIdx fusion arguments don't match"
-
-evalLoopBody :: LoopBody pinputs tinputs outputs
-             -> ValArr pinputs
-             -> ValArr tinputs'
-             -> ValArr outputs'
-             -> IdxSpaceTransform tinputs tinputs'
-             -> IdxSpaceTransform outputs outputs'
-             -> IO ()
-evalLoopBody Base _ x y Id Id = assign Id x y
-
-evalLoopBody (Drop     lb) p (PushEnv ts t) (PushEnv os o) Id       Id       = assignArr         0     t o >> evalLoopBody lb p ts os Id Id
-evalLoopBody (Drop     lb) p (PushEnv ts t) (PushEnv os o) (Fn i a) Id       = assignArr         (-i)  t o >> evalLoopBody lb p ts os a  Id
-evalLoopBody (Drop     lb) p (PushEnv ts t) (PushEnv os o) Id       (Fn j b) = assignArr         j     t o >> evalLoopBody lb p ts os Id b
-evalLoopBody (Drop     lb) p (PushEnv ts t) (PushEnv os o) (Fn i a) (Fn j b) = assignArr         (i-j) t o >> evalLoopBody lb p ts os a  b
-evalLoopBody (Take lb' lb) p (PushEnv ts t) (PushEnv os o) Id       Id       = evalLoopBody' lb' 0 0 p t o >> evalLoopBody lb p ts os Id Id
-evalLoopBody (Take lb' lb) p (PushEnv ts t) (PushEnv os o) (Fn i a) Id       = evalLoopBody' lb' i 0 p t o >> evalLoopBody lb p ts os a  Id
-evalLoopBody (Take lb' lb) p (PushEnv ts t) (PushEnv os o) Id       (Fn j b) = evalLoopBody' lb' 0 j p t o >> evalLoopBody lb p ts os Id b
-evalLoopBody (Take lb' lb) p (PushEnv ts t) (PushEnv os o) (Fn i a) (Fn j b) = evalLoopBody' lb' i j p t o >> evalLoopBody lb p ts os a  b
-
-evalLoopBody (Take _ _) _ EmptyEnv _ x _ = case x of {} -- There is no IdxSpaceTransform
-evalLoopBody (Drop   _) _ EmptyEnv _ x _ = case x of {} -- possible for these cases,
-evalLoopBody (Take _ _) _ _ EmptyEnv _ x = case x of {} -- so this satisfies GHC's
-evalLoopBody (Drop   _) _ _ EmptyEnv _ x = case x of {} -- incomplete pattern check. :)
-
-
-
-evalLoopBody' :: LoopBody' pinputs (Array sh1 e1) (Array sh2 e2)
+evalLB' :: LoopBody' pinputs (Array sh1 e1) (Array sh2 e2)
               -> Int -- offset input  index
               -> Int -- offset output index
               -> ValArr pinputs
               -> Array sh1' e1
               -> Array sh2' e2
               -> IO ()
-evalLoopBody' (OneToOne sref f) inoff outoff p (Array _ a) (Array _ b) = do
+evalLB' (OneToOne sref f) inoff outoff p (Array _ a) (Array _ b) = do
   s <- readIORef sref
-  let (res, s') = (`runState` s) . f p . toElt $ unsafeIndexArrayData a inoff
+  a' <- unsafeReadArrayData a inoff
+  let (res, s') = (`runState` s) . f p . toElt $ a'
   writeIORef sref s'
   unsafeWriteArrayData b outoff $ fromElt res
 
-evalLoopBody' (ManyToOne n sref acc ret) inoff outoff p (Array _ a) (Array _ b) = do
+evalLB' (ManyToOne sref n acc ret) inoff outoff p (Array _ a) (Array _ b) = do
   (s, i) <- readIORef sref
-  let s' = (`execState` s) . acc p . toElt $ unsafeIndexArrayData a inoff
-  let i' = i + 1
+  a' <- unsafeReadArrayData a inoff
+  let s' = (`execState` s) . acc p . toElt $ a'
+  let i' = (i + 1) `mod` n
   writeIORef sref (s', i')
-  when (i' == n) $ do
+  when (i' == 0) $ do
     let res = evalState ret s'
     unsafeWriteArrayData b outoff $ fromElt res
 
-
-
-fuseHorizontal
-  :: HorizontalFusion a b c
-  -> ValArr a
-  -> ValArr b
-  -> ValArr c
-fuseHorizontal Done _ _ = EmptyEnv
-fuseHorizontal (Righty r) EmptyEnv (b `PushEnv` x) = PushEnv (fuseHorizontal r EmptyEnv b) x
-fuseHorizontal (Lefty l) (a `PushEnv` x) b         = PushEnv (fuseHorizontal l a        b) x
-
+evalLB' (OneToMany sref n f) inoff outoff p (Array _ a) (Array _ b) = do
+  (s', bs', i) <- readIORef sref
+  (bs, s) <- if i==0 then do
+    a' <- unsafeReadArrayData a inoff
+    return . (`runState` s') . f p . toElt $ a'
+      else return (bs', s')
+  let i' = (i+1) `mod` n
+  writeIORef sref (s, tail bs, i')
+  unsafeWriteArrayData b outoff . fromElt . head $ bs
 
 
 -- assign the value 'from' to the internals of 'to'
 assign :: IdxSpaceTransform a b -> ValArr a -> ValArr b -> IO ()
-assign _ EmptyEnv EmptyEnv = return ()
+assign Id EmptyEnv EmptyEnv = return ()
 assign Id       (PushEnv a from) (PushEnv b to) = assignArr 0 from to >> assign Id a b
 assign (Fn i f) (PushEnv a from) (PushEnv b to) = assignArr i from to >> assign f  a b
-
+assign (Skip f) a (PushEnv b _) = assign f a b
 
 -- TODO specialise if sh1 and sh2 match (and the offset is 0)
 assignArr :: Int -> Array sh1 a -> Array sh2 a -> IO ()
